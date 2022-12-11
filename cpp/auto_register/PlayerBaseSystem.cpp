796f5ce858f1c6132a59d20d1f9ab20143a2a47f
cxl <c.x.l@live.com>
2022-12-11 23:29:46 +0800

    测试emacs git提交

--- cpp/auto_register/PlayerBaseSystem.cpp
+++ cpp/auto_register/PlayerBaseSystem.cpp
@@ -1,118 +1,112 @@
 #include <iostream>
 
-template<typename T>
-class Singleton
-{
+template <typename T> class Singleton {
 public:
-    static void CreateInstance()
-    {
-        std::cout << "CreateInstance" << std::endl;
-        if (instance == nullptr) instance = new T();
+  static void CreateInstance() {
+    std::cout << "CreateInstance" << std::endl;
+    if (instance == nullptr)
+      instance = new T();
+  }
+
+  static void DestroyInstance() {
+    if (instance != nullptr)
+      delete instance;
+    instance = nullptr;
+  }
+
+  static inline T *Instance() {
+    std::cout << "Instance" << std::endl;
+    if (instance == nullptr) {
+      CreateInstance();
     }
 
-    static void DestroyInstance()
-    {
-        if (instance != nullptr) delete instance;
-        instance = nullptr;
-    }
-
-    static inline T* Instance()
-    {
-        std::cout << "Instance" << std::endl;
-        if (instance == nullptr) { CreateInstance(); }
-
-        return instance;
-    }
+    return instance;
+  }
 
 protected:
-    Singleton() {}
-    ~Singleton() {}
-    static T* instance;
+  Singleton() {}
+  ~Singleton() {}
+  static T *instance;
 };
 
-template<typename T>
-T* Singleton<T>::instance;
+template <typename T> T *Singleton<T>::instance;
 
-class IPlayerSystemInterface
-{
+class IPlayerSystemInterface {
 public:
-    IPlayerSystemInterface() { bIsReg = false; }
+  IPlayerSystemInterface() { bIsReg = false; }
 
-    virtual ~IPlayerSystemInterface() {}
+  virtual ~IPlayerSystemInterface() {}
 
-    virtual int OnInit() = 0;
+  virtual int OnInit() = 0;
 
-    bool IsReg() { return bIsReg; }
-    void MarkReg() { bIsReg = true; }
+  bool IsReg() { return bIsReg; }
+  void MarkReg() { bIsReg = true; }
 
 private:
-    bool bIsReg;
+  bool bIsReg;
 };
 
-class PlayerSystemMgr : public Singleton<PlayerSystemMgr>
-{
+class PlayerSystemMgr : public Singleton<PlayerSystemMgr> {
 public:
-    PlayerSystemMgr() {}
-    ~PlayerSystemMgr(){};
+  PlayerSystemMgr() {}
+  ~PlayerSystemMgr(){};
 
-    bool RegisterSystemInterface(IPlayerSystemInterface* iSystem)
-    {
-        if (iSystem == nullptr) { return false; }
-        if (iSystem->IsReg()) { return false; }
-
-        iSystem->MarkReg();
-        arrayInterface[curIndex] = iSystem;
-        ++curIndex;
-
-        std::cout << "PlayerSystemMgr " << curIndex << std::endl;
-        return true;
+  bool RegisterSystemInterface(IPlayerSystemInterface *iSystem) {
+    if (iSystem == nullptr) {
+      return false;
+    }
+    if (iSystem->IsReg()) {
+      return false;
     }
 
+    iSystem->MarkReg();
+    arrayInterface[curIndex] = iSystem;
+    ++curIndex;
+
+    std::cout << "PlayerSystemMgr " << curIndex << std::endl;
+    return true;
+  }
+
 private:
-    IPlayerSystemInterface* arrayInterface[100];
-    int                     curIndex;
+  IPlayerSystemInterface *arrayInterface[100];
+  int curIndex;
 };
 
-template<typename T>
-class PlayerBaseSystem : public IPlayerSystemInterface, public Singleton<T>
-{
+template <typename T>
+class PlayerBaseSystem : public IPlayerSystemInterface, public Singleton<T> {
 private:
-    struct STHelper{
-        STHelper() {
-            std::cout << "helper" << std::endl;
-            PlayerSystemMgr::Instance()->RegisterSystemInterface(T::Instance());
-        }
-
-        void DoNothing() {}
-    };
-    static STHelper helper;
-
-public:
-    PlayerBaseSystem()
-    {
-        std::cout << "PlayerBaseSystem" << std::endl;
-        helper.DoNothing();
+  struct STHelper {
+    STHelper() {
+      std::cout << "helper" << std::endl;
+      PlayerSystemMgr::Instance()->RegisterSystemInterface(T::Instance());
     }
 
-    virtual ~PlayerBaseSystem() {}
+    void DoNothing() {}
+  };
+  static STHelper helper;
+
+public:
+  PlayerBaseSystem() {
+    std::cout << "PlayerBaseSystem" << std::endl;
+    helper.DoNothing();
+  }
+
+  virtual ~PlayerBaseSystem() {}
 };
-template<typename T>
+template <typename T>
 typename PlayerBaseSystem<T>::STHelper PlayerBaseSystem<T>::helper;
 
-class A : public PlayerBaseSystem<A>
-{
+class A : public PlayerBaseSystem<A> {
 public:
-    A() { std::cout << "create A" << std::endl; }
+  A() { std::cout << "create A" << std::endl; }
 
-    int OnInit() override
-    {
-        std::cout << "A::OnInit()" << std::endl;
-        return 0;
-    }
+  int OnInit() override {
+    std::cout << "A::OnInit()" << std::endl;
+    return 0;
+  }
 };
 
-int main(int argc, char* argv[])
-{
-    A::Instance()->OnInit();
-    return 0;
+int main(int argc, char *argv[]) {
+  A::Instance()->OnInit();
+  return 0;
 }
